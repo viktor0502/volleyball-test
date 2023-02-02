5753c611d5b15215f8439c7f868d3b9a085820e4
Viktor <vik.more0502@gmail.com>
2023-02-02 20:15:10 +0200

    1

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -63,3 +63,50 @@ function accordion(selector) {
 accordion("accordion__btn");
 
 
+// burger__menu
+
+function burgerMenu(burgerBtnSelector, navMenuSelector, linksSelector) {
+  const burger = document.querySelectorAll("." + burgerBtnSelector),
+    navMenu = document.querySelectorAll("." + navMenuSelector),
+    link = document.querySelectorAll("." + linksSelector);
+
+  const scrollLock = () => {
+      document.body.style.overflow = "hidden";
+    },
+    scrollUnclock = () => {
+      document.body.style.overflow = "";
+    },
+    menuOpen = () => {
+      navMenu.forEach((menu) => {
+        menu.classList.add(navMenuSelector + "--active");
+        scrollLock();
+      });
+    },
+    menuClose = () => {
+      navMenu.forEach((menu) => {
+        menu.classList.remove(navMenuSelector + "--active");
+        scrollUnclock();
+      });
+    };
+
+  burger.forEach((burger) => {
+    burger.addEventListener(`click`, () => {
+      burger.classList.toggle(burgerBtnSelector + "--active");
+      if (burger.classList.contains(burgerBtnSelector + "--active")) {
+        menuOpen();
+      } else {
+        menuClose();
+      }
+    });
+  });
+  link.forEach((i) => {
+    i.addEventListener(`click`, () => {
+      burger.forEach((burger) => {
+        burger.classList.remove(burgerBtnSelector + "--active");
+        menuClose();
+      });
+    });
+  });
+}
+
+burgerMenu('nav__burger-menu', 'nav__menu', 'nav__link')
